9d965e7c4903f5e6355e72b64ae2932830ca28ee
JMoloughney <github email address>
2018-04-23 21:23:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file
+
+
+// for (const char of 'Hello, world!') {
+//   console.log(char);
+// }
+
+
+// for (const <KEY> in <OBJECT>) {
+//   // Code in the statement body
+// }
+
+function findMatching(drivers, d) {
+  const matching = drivers.filter(function(driver){return driver.toUpperCase() === d.toUpperCase()});
+  return matching
+};
+
+function fuzzyMatch(drivers, d2) {
+  const matching = drivers.filter(function(driver){return driver[0] === d2[0] && driver[1] === d2[1]});
+  return matching
+};
+
+function matchName(drivers, d){
+  const matching = drivers.filter(function(driver){return driver.name === d});
+  return matching
+};
